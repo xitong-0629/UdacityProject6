d332357c73b474f90d28063ff3b427613c92746a
Andy Gu <43463033+zjuguyunzhe@users.noreply.github.com>
2020-02-17 23:50:59 +0800

    first commit

---
+++ contracts/ExerciseC6A.sol
@@ -0,0 +1,117 @@
+pragma solidity >=0.5.0 <0.7.0;
+
+contract ExerciseC6A {
+
+    /********************************************************************************************/
+    /*                                       DATA VARIABLES                                     */
+    /********************************************************************************************/
+
+    uint constant M = 2;
+    address[] multiCalls = new address[](0);
+
+    struct UserProfile {
+        bool isRegistered;
+        bool isAdmin;
+    }
+
+    address private contractOwner;                  // Account used to deploy contract
+    mapping(address => UserProfile) userProfiles;   // Mapping for storing user profiles
+
+    bool private operational = true;
+
+    /********************************************************************************************/
+    /*                                       EVENT DEFINITIONS                                  */
+    /********************************************************************************************/
+
+    // No events
+
+    /**
+    * @dev Constructor
+    *      The deploying account becomes contractOwner
+    */
+    constructor() public {
+        contractOwner = msg.sender;
+    }
+
+    /********************************************************************************************/
+    /*                                       FUNCTION MODIFIERS                                 */
+    /********************************************************************************************/
+
+    // Modifiers help avoid duplication of code. They are typically used to validate something
+    // before a function is allowed to be executed.
+
+    /**
+    * @dev Modifier that requires the "ContractOwner" account to be the function caller
+    */
+    modifier requireContractOwner() {
+        require(msg.sender == contractOwner, "Caller is not contract owner");
+        _;
+    }
+
+    /**
+    * @dev Modifier that requires the "operational" boolean variable to be "true"
+    *      This is used on all state changing functions to pause the contract in
+    *      the event there is an issue that needs to be fixed
+    */
+    modifier requireIsOperational() {
+        require(operational, "Contract is currently not operational");
+        _;  // All modifiers require an "_" which indicates where the function body will be added
+    }
+
+    /********************************************************************************************/
+    /*                                       UTILITY FUNCTIONS                                  */
+    /********************************************************************************************/
+
+   /**
+    * @dev Check if a user is registered
+    *
+    * @return A bool that indicates if the user is registered
+    */
+    function isUserRegistered(address account) external view returns(bool) {
+        require(account != address(0), "'account' must be a valid address.");
+        return userProfiles[account].isRegistered;
+    }
+
+    /**
+    * @dev Get operating status of contract
+    *
+    * @return A bool that is the current operating status
+    */
+    function isOperational() public view returns(bool) {
+        return operational;
+    }
+
+    /********************************************************************************************/
+    /*                                     SMART CONTRACT FUNCTIONS                             */
+    /********************************************************************************************/
+
+    function registerUser(address account, bool isAdmin) external requireIsOperational requireContractOwner {
+        require(!userProfiles[account].isRegistered, "User is already registered.");
+        userProfiles[account] = UserProfile({isRegistered: true, isAdmin: isAdmin});
+    }
+
+    /**
+    * @dev Sets contract operations on/off
+    *
+    * When operational mode is disabled, all write transactions except for this one will fail
+    */
+    function setOperatingStatus(bool mode) external requireIsOperational requireContractOwner {
+        require(mode != operational, "New mode must be different from existing mode");
+        require(userProfiles[msg.sender].isAdmin, "Caller is not an admin");
+
+        bool isDuplicate = false;
+        for(uint c = 0; c < multiCalls.length; c++) {
+            if (multiCalls[c] == msg.sender) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        require(!isDuplicate, "Caller has already called this function.");
+
+        multiCalls.push(msg.sender);
+        if (multiCalls.length >= M) {
+            operational = mode;
+            multiCalls = new address[](0);
+        }
+    }
+}
