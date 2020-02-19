4b444a1b8cb2d3613353b1f220fdf170070e838b
Andy Gu <43463033+zjuguyunzhe@users.noreply.github.com>
2020-02-20 00:02:23 +0800

    Exerciese B first commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,5 +3,7 @@ const ExerciseC6A = artifacts.require("ExerciseC6A");
 module.exports = function(deployer) {
 
     deployer.deploy(ExerciseC6A);
-
+    deployer.deploy(ExerciseC6B).then(function() {
+        console.log("Deployed Contract Address:", ExerciseC6B.address);
+    });
 }
